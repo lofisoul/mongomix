c0fe86f886f5d0b989b5023c960eda6dae1818a9
lofisoul <jason.nolfi@gmail.com>
2017-06-16 17:44:04 -0500

    Fixed app.js

--- public/js/app.js
+++ public/js/app.js
@@ -19,21 +19,3 @@ $(document).on('click','#noteBtn', function(){
      }
   });
 });
-
-// $(document).on('click','.note-del-btn', function(){
-//   var thisId = $(this).attr('data-btnID');
-//   //$('.comment-window').empty();
-//
-//   //ajax
-//   $.ajax({
-//     method: 'POST',
-//     url:'/delete/' + thisId
-//   })
-//   .done(function(data){
-//     console.log(data);
-//     for(i=0; i<data.notes.length; i++) {
-//      console.log(data.notes[i]._id);
-//      $('.comment-window').append('<div class="comment-item" data-id="'+data.notes[i]._id+'">'+data.notes[i].body+'<form class="remove-note" action="/delete/'+data.notes[i]._id+'" method="POST"><input type="hidden" name="_id"><button class="btn btn-danger note-del-btn" data-btnID="'+data.notes[i]._id+'" type="submit"><i class="fa fa-trash-o" aria-hidden="true"></i></button></form></div>');
-//      }
-//   });
-// });
