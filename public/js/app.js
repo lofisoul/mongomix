51d2de20f45c1089e070582cc4e7ec5b2bc4ffdd
lofisoul <jason.nolfi@gmail.com>
2017-06-16 19:04:28 -0500

    Updated css styling and handlebars

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ $(document).on('click','#noteBtn', function(){
     console.log(data);
     for(i=0; i<data.notes.length; i++) {
      console.log(data.notes[i]._id);
-     $('.comment-window').append('<div class="comment-item" data-id="'+data.notes[i]._id+'">'+data.notes[i].body+'<form class="remove-note" action="/delete/'+data.notes[i]._id+'" method="POST"><input type="hidden" name="note"><button class="btn btn-danger note-del-btn" data-btnID="'+data.notes[i]._id+'" type="submit"><i class="fa fa-trash-o" aria-hidden="true"></i></button></form></div>');
+     $('.comment-window').append('<div class="comment-item clearfix" data-id="'+data.notes[i]._id+'"><div class="note-txt">'+data.notes[i].body+'</div><form class="remove-note" action="/delete/'+data.notes[i]._id+'" method="POST"><input type="hidden" name="note"><button class="btn btn-danger note-del-btn" data-btnID="'+data.notes[i]._id+'" type="submit"><i class="fa fa-trash-o" aria-hidden="true"></i></button></form></div>');
      }
   });
 });
